7d1d0c4379a4034bf8f0a1cdf9a66ad9906d172c
Tremmmmm <153413955+Tremmmmm@users.noreply.github.com>
2025-11-12 16:40:04 +0700

    fix

--- foodfast-react-ui/src/utils/format.js
+++
@@ -1,2 +0,0 @@
-export const formatVND = (n) =>
-  (Number(n) || 0).toLocaleString("vi-VN", { style: "currency", currency: "VND" });
--- foodfast-react-ui/src/utils/validators.js
+++
@@ -1,6 +0,0 @@
-export const PHONE_VN = /^(0|\+84)(3|5|7|8|9)\d{8}$/;
-
-export const isPhoneVN = (s) => {
-  const v = String(s ?? "").trim();
-  return PHONE_VN.test(v);
-};
